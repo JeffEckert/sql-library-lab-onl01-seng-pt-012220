7b3ba3275c49cfa5d65ffeec774dbbd6ce8d3cb7
Jeffery Eckert <bromodo@gmail.com>
2020-03-26 23:46:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(*)
   FROM characters
   GROUP BY species
-  ORDER BY "
+  ORDER BY COUNT(species) DESC
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
